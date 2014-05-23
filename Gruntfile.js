26d6ec4a5f805529b498041084681502410b7f40
Nick Visutsithiwong <nvisutsithiwong@hq.bill.com>
2014-05-23 14:47:10 -0700

    changed custom task to multitask

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,17 +9,60 @@ module.exports = function(grunt) {
 	// grunt.loadNpmTasks('grunt-contrib-clean');
 	// grunt.loadNpmTasks('grunt-contrib-copy');
 
+	// replaced in initConfig with the multi-target
+ 	// version: {
+	//     options: {
+	//         buildNumber: "1.0." + (+new Date())
+	//     }
+	// }
+
+
 	grunt.initConfig({
 		clean: ['./dist'],
 	    copy: {
 	        release: {
 	            files: [ { src: './**', dest: './dist/' } ]
 	        }
-	    }
+	    },
+		version: {
+		    release: {
+		        options: {
+		            buildNumber: "1.0." + (+new Date())
+		        }
+		    },
+		    dev: {
+		        options: {
+		            buildNumber: function() {
+		                var package = JSON.parse(grunt.file.read('./package.json'));
+		                var chunks = package.version.split('.');
+		                chunks[1] = parseInt(chunks[1], 10) + 1;
+		                return chunks.join('.') + '-pre';
+		            }()
+		        }
+		    }
+		}
 	});
 
 	// TODO: so how does this know about grunt-contrib-*?
 	require('load-grunt-tasks')(grunt);
 
-	grunt.registerTask('default', ['clean', 'copy']);
+	// replaced registerTask with multi
+	//
+	// puts 1.0. and milliseconds as version into package.json
+	// grunt.registerTask('version', 'Add build number to the package.json', function() {
+	// 	var package = JSON.parse(grunt.file.read('./package.json'));
+	// 	package.version = this.options().buildNumber;
+
+	// 	grunt.file.write('./dist/package.json', JSON.stringify(package, null, 2));
+	// });
+
+	// puts 1.0. and milliseconds as version into package.json
+	grunt.registerMultiTask('version', 'Add build number to the package.json', function() {
+		var package = JSON.parse(grunt.file.read('./package.json'));
+		package.version = this.options().buildNumber;
+
+		grunt.file.write('./dist/package.json', JSON.stringify(package, null, 2));
+	});
+
+	grunt.registerTask('default', ['clean', 'copy', 'version:release']);
 };
